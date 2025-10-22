fe552f104b17601028c064eda14f98e2a4afea71
Mike <kononov.mixa2045@gmail.com>
2025-10-22 19:40:32 +0500

    Задание 5.9

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,7 +1,19 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { CreateOfferDto, OfferEntity } from './index.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { City } from '../../types/city.type.js';
 
-export interface OfferService {
+export interface IOfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  addComment(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  getFavourites(userId: string): Promise<DocumentType<OfferEntity>[]>;
+  addToFavourites(offerId: string, userId: string): Promise<DocumentType<OfferEntity> | null>;
+  deleteFromFavourites(offerId: string, userId: string): Promise<DocumentType<OfferEntity> | null>;
+  calculateRating(offerId: string): Promise<void>;
+  findPremiumInCity(city: City): Promise<DocumentType<OfferEntity>[]>;
 }
